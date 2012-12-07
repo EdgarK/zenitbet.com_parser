040cd46e0dce2f951b9c7e4477fa3ba577206967
EdgarK <kedgark@gmail.com>
2012-12-07 17:38:02 +0200

    bugfix

--- script.rb
+++ script.rb
@@ -4,7 +4,6 @@ require 'net/http'
 require 'nokogiri'
 
 
-
 def create_or_update_bet(bookmaker_event, period, bet_variation, value, koef, direct_link = nil, commission = 0, is_lay = false, market = nil)
   puts "bookmaker_event => #{bookmaker_event}, period => #{period}, bet_variation => #{bet_variation}, value => #{value}, koef => #{koef}"
 end
@@ -13,22 +12,75 @@ end
 
 def get_bookmaker_event(*args)
   result = "#{ @count }."
-  %w(sport, home_team, away_team, date, time).each_with_index{|name, i| result << " #{name} => '#{args[i]}' " }
+  %w(sport, home_team, away_team, date, time).each_with_index { |name, i| result << " #{name} => '#{args[i]}' " }
   puts result
   @count += 1
 end
 
 
+class Translator
+  NAME_TRANSLATION = {'П1' => 'ML1', 'Х' => 'X', 'П2' => '2', '1Х' => '1X', '12' => '12', 'Х2' => 'X2',"Кф1" => "F1", "Кф2" => "F2", "К1" => "F1", "К2" => "F2", "Бол" => "TO", "Мен" => "TU"}
+  SPORT_NAME_TRANSLATION = {'Футбол' => 'soccer', 'Хоккей' => 'hockey', 'Теннис' => 'tennis', 'Снукер' => 'snooker', 'Гандбол' => 'handball', 'Волейбол' => 'volleyball', 'Баскетбол' => 'basketball','Настольный теннис' => 'table tennis'}
+  DOUBLED_VALUES = {"Кф1" => "Ф1", "Кф2" => "Ф2", "К1" => "Ф1", "К2" => "Ф2", "Бол" => "Тот", "Мен" => "Тот"}
+
+  def initialize
+    @sport = nil
+  end
+
+  def translate_game(name)
+    raise "No translation for game name #{name.inspect}" unless SPORT_NAME_TRANSLATION.keys.include?(name)
+    SPORT_NAME_TRANSLATION[name]
+  end
+
+  def sport=(name=nil)
+    if SPORT_NAME_TRANSLATION.keys.include?(name)
+      @sport = SPORT_NAME_TRANSLATION[name]
+    elsif SPORT_NAME_TRANSLATION.values.include?(name) || name.nil?
+      @sport = name
+    end
+    if name && !self.instance_variable_get("@#{@sport}")
+      self.instance_variable_set("@#{@sport}", {})
+    end
+
+  end
+
+  def sport()
+    @sport
+  end
+
+  def get_values(key, values)
+    raise "No translation for #{key.inspect}" unless self.has?(key)
+    translated = if val = self.instance_variable_get("@#{@sport}")[key]
+            val
+          else
+            NAME_TRANSLATION[key]
+          end
+
+    [translated, values[DOUBLED_VALUES[key]], values[key]] if translated
+  end
+
+  def has?(key)
+    (self.instance_variable_get("@#{@sport}").keys + NAME_TRANSLATION.keys + DOUBLED_VALUES.keys + DOUBLED_VALUES.values).include? key
+  end
+
+  def []=(key, val)
+    self.instance_variable_get("@#{@sport}")[key] = val
+  end
+end
+
+translation = Translator.new()
+translation.sport = "soccer"
+translation['П1'] = '1'
 
 
-NAME_TRANSLATION = {'П1' => '1', 'Х' => 'X', 'П2' => '2', '1Х' => '1X', '12' => '12', 'Х2' => 'X2'}
-SPORT_NAME_TRANSLATION = {'Футбол' => 'soccer', 'Хоккей' => 'hockey', 'Теннис' => 'tennis', 'Снукер' => 'snooker', 'Гандбол' => 'handball', 'Волейбол' => 'volleyball', 'Баскетбол' => 'basketball'}
-#americanfootball
 
 
 def fetch_html
   http = Net::HTTP.new('zenitbet.com', 80)
-  cookies = "PHPSESSID=8s61dkn4c2d8rm48ccl064agb3; __utma=170375595.704838195.1354790217.1354790217.1354794732.2; __utmc=170375595; __utmz=170375595.1354790217.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); WRUID=0; basket_style=0; __utmb=170375595.4.10.1354794732; _ym_visorc=b"
+  request = (Net::HTTP::Get.new("/line/setdata?onlyview=1&all=1&game=&live=1&timeline=0&ross=1"))
+  cookies = http.request(request).get_fields('set-cookie')
+  cookies.map! { |c| c.split('; ')[0] }
+  cookies = cookies.join('; ')
   request = Net::HTTP::Post.new "/line/loadline"
   request.set_form_data({'live' => 1})
   request.initialize_http_header({'Cookie' => cookies, 'charset' => "windows-1251"})
@@ -36,80 +88,79 @@ def fetch_html
   Nokogiri::HTML(http.request(request).body.force_encoding("windows-1251").encode('utf-8'), nil, 'utf-8')
 end
 
+
+def get_labels(heads)
+  labels = []
+  heads.each { |head| labels << head.text.gsub(/(^  *)|(  *$)/, "") unless head.text == "" }
+  labels
+end
+
 lines = {}
 fetch_html.css("div.b-sport").each do |sport|
   name = sport.css("span.b-league-name-label")[0].text.split(". ")[1]
   lines[name] ||= []
-  line = {}
-  sport.children.each do |match|
-    if match['class'] == 'b-league'
-      labels = []
-      match.css('table.t-league').children.each do |row|
-        if row.name == 'thead'
-          row.css('th').each do |column|
-            labels << column.text
-          end
-        else
-          line['periods'] ||= {}
-          line['periods']['main_time'] ||= {}
-          row.css('tr.o').css('td').each_with_index do |column, i|
-            if ["Событие", "Дата"].include?(labels[i])
-              line[labels[i]] = column.text.gsub(/(^  *)|(  *$)/, "") if !["", " ", "  "].include?(column.text) and i < labels.length
-            else
-              line['periods']['main_time'][labels[i]] = column.text.gsub(/(^  *)|(  *$)/, "") if !["", " ", "  "].include?(column.text) and i < labels.length
-            end
-          end
-          ext_labels = Array(row.css('tr.t-league__ross').css('table > thead > tr > th')[1..-1]).map { |val| val.text }
-          row.css('tr.t-league__ross').css('table > tbody > tr').each do |tr|
-            time = tr.css('td > b').text
-            line['periods'][time] ||= {}
-            tr.css('td')[1..-1].each_with_index do |val, i|
-              line['periods'][time][ext_labels[i]] = val.text.to_f unless ["", " ", "  "].include?(val.text)
-            end
-          end
-          Array(row.css('tr.t-league__ross > td > div > div')).each do |bla|
-            text = bla.text.gsub(bla.css('b')[0].text, "")
-            line[bla.css('b')[0].text] = text unless ["", " ", "  "].include?(text)
-          end
-        end
+  (sport.children.length / 2).times do |i|
+    line = {}
+    line['periods'] = {}
+    line['periods']['main_line'] = {}
+    line['additional_totals'] = {}
+    line['league_name'] = sport.css('span.b-league-name-label')[i].text.split(". ")[2..-1].join('. ')
+    league_body = sport.css('div.b-league')[i]
+    matches = league_body.css('table.t-league')
+    (matches.children.length / 2).times do |match_num|
+      match_head = matches.css('thead')[match_num]
+      labels = get_labels(match_head.css('th')[2..-1])
+      match_body = league_body.css('table.t-league > tbody')[match_num]
+      main_line = match_body.css('tr.o, tr.e')
+
+      main_line.css('td')[2..-1].each_with_index { |column, index| line['periods']['main_line'][labels[index]] = column.text if !['', ' '].include?(column.text) && labels[index] }
+
+      line['time'] = main_line.css('td')[0].text.gsub(/(^  *)|(  *$)/, "")
+      line['event'] = main_line.css('td')[1].text
+      line["home_team"], line["away_team"] = line['event'].split(" - ").map { |val| val.gsub(/(^ *)|( ?[0-9:*]+.*)|( *$)/, "") }
+
+      additional_info = match_body.css('tr.t-league__ross > td > div')
+      additional_lines = additional_info.css('table')
+      additional_totals = additional_info.css('div')
+      labels = get_labels(additional_lines.css('th'))
+      additional_lines.css('tbody > tr').each do |row|
+        line_name = row.css('td')[0].text.gsub(/[^0-9]+/, '')
+        line['periods'][line_name] = {}
+        row.css('td')[1..-1].each_with_index { |column, index| line['periods'][line_name][labels[index]] = column.text if !['', ' '].include?(column.text) }
       end
-      line["home_team"], line["away_team"] = line["Событие"].split(" - ").map { |val| val.gsub(/(^ *)|( ?[0-9:*]+.*)|( *$)/, "") }
-    else
-      line['league'] = match.css('span.b-league-name-label').text.split(". ")[2..-1].join(". ")
+      additional_totals.each do |total|
+        total_name = total.text.split(':')[0]
+        total_value = Array(total.text.split(':')[1..-1]).join(':')
+        line['additional_totals'][total_name] = total_value
+      end
     end
 
-    lines[name] << line
+    lines[name] << line.clone
   end
+  bla = 0
 end
 
 
-
-
-
 lines.each do |key, val|
-  game = SPORT_NAME_TRANSLATION[key]
+  game = translation.translate_game(key)
+  translation.sport = game
   if game
     val.each do |match|
-      bookmaker_event = get_bookmaker_event(game, match["home_team"], match["away_team"], Time.now.to_s, match["Дата"])
+      bookmaker_event = get_bookmaker_event(game, match["home_team"], match["away_team"], Time.now.to_s, match['time'])
       match['periods'].each do |name, period|
-        if name == "main_time"
-          per = "0"
+        if name == "main_line"
+          per = (%w(basketball tennis).include? game )? '-1' : "0"
         else
           per = name.gsub(/-.+/, "")
         end
-        NAME_TRANSLATION.each do |k, v|
-          create_or_update_bet(bookmaker_event, per, v, nil, period[k]) if period[k]
+
+        period.each do |k, v|
+          if k && (values = translation.get_values(k, period))
+            create_or_update_bet(bookmaker_event, per, *values)
+          end
+
         end
 
-        create_or_update_bet(bookmaker_event, per, "F1", period["Ф1"], period["Кф1"]) if period["Ф1"] && period["Кф1"]
-        create_or_update_bet(bookmaker_event, per, "F2", period["Ф2"], period["Кф2"]) if period["Ф2"] && period["Кф2"]
-        create_or_update_bet(bookmaker_event, per, "F1", period["Ф1"], period["К1"]) if period["Ф1"] && period["К1"]
-        create_or_update_bet(bookmaker_event, per, "F2", period["Ф2"], period["К2"]) if period["Ф2"] && period["К2"]
-
-        if period['Тот']
-          create_or_update_bet(bookmaker_event, per, 'TO', period['Тот'], period['Бол'])
-          create_or_update_bet(bookmaker_event, per, 'TU', period['Тот'], period['Мен'])
-        end
       end
     end
   end
